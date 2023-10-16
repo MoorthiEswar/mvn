570371b0ba7924cfe7473b053d77827ffe02e3cf
Dell <Dell@DESKTOP-ILOVP9M>
2023-10-16 12:56:44 +0530

    First commit

---
+++ mavenproject/src/test/java/com_actitime_testscripts/createcustomer.java
@@ -0,0 +1,12 @@
+package com_actitime_testscripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class createcustomer {
+     @Test
+	public void testcustomer()
+	{
+		Reporter.log("create customer",true);
+	}
+}
